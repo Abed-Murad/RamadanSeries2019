d3f4ebf5cf3f6bdd3c3f8638d5efbbbbde559e27
Abed Murad <abdallah.Murad@mail.com>
2019-01-29 13:25:53 +0200

    Add YoutubeView Library and implement activity_episode

---
+++ app/src/main/java/com/am/ramadanseries2019/activity/EpisodeActivity.java
@@ -0,0 +1,15 @@
+package com.am.ramadanseries2019.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.am.ramadanseries2019.R;
+
+public class EpisodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_episode);
+    }
+}
